2af543ecf0be103e994121243391d279d6ddd47e
Cornelius Bergen <cornelius@matchboxcreative.com>
2016-12-14 12:12:27 -0800

    fix redis

--- settings/index.js
+++ settings/index.js
@@ -22,6 +22,7 @@ module.exports = {
   workingDirectory: path.join(__dirname, "..", "tmp"),
   storagePath: path.join(__dirname, "..", "media"),
   redisHost: "127.0.0.1",
+  worker: true,
   fonts: [
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Regular.ttf") },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Light.ttf"), weight: 300 },
