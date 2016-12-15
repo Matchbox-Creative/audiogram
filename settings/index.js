8ab33c198f5ed383ba45797d6d2207ef54a773b5
Cornelius Bergen <cornelius@matchboxcreative.com>
2016-12-14 17:03:35 -0800

    stop using redis

--- settings/index.js
+++ settings/index.js
@@ -21,8 +21,8 @@ var path = require("path");
 module.exports = {
   workingDirectory: path.join(__dirname, "..", "tmp"),
   storagePath: path.join(__dirname, "..", "media"),
-  redisHost: "127.0.0.1",
-  worker: true,
+  // redisHost: "127.0.0.1",
+  // worker: true,
   fonts: [
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Regular.ttf") },
     { family: "Source Sans Pro", file: path.join(__dirname, "fonts", "SourceSansPro-Light.ttf"), weight: 300 },
